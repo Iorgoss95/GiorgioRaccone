4a9ad12b1476e193aa5741f27a3b95493b5dcb14
giorgio <giorgio.raccone@finconsgroup.com>
2021-07-08 17:15:07 +0200

    theme color test 3

--- js/script.js
+++ js/script.js
@@ -1,22 +1,34 @@
+//javascript file dor my personal site
 
+
+
+//get backgrund color and set to content property of meta tag
+let color = window.getComputedStyle(document.querySelector(".top-container")).backgroundColor;
+document.querySelector("meta[name='theme-color']").setAttribute("content", color);
 
 const btn = document.querySelector(".btn-toggle");
 let i = 2;
-
-console.log("pre button  " + i);
 btn.addEventListener("click", function () {
+
     document.body.classList.remove("theme-" + (i - 1))
-    console.log("removed " + (i - 1), "add " + i);
     document.body.classList.add("theme-" + i);
+
+    //get backgrund color and set to content property of meta tag
+    let color = window.getComputedStyle(document.querySelector(".top-container")).backgroundColor;
+    document.querySelector("meta[name='theme-color']").setAttribute("content", color);
+
     if (i > 6) {
         //i > of number oh themes in _colors.scss file
         document.body.classList.remove("theme-" + (i))
-        console.log("remove" + i);
         i = 2;
         document.body.classList.add("theme-" + (i - 1));
-        console.log("add" + (i - 1));
     }
     else {
         i++;
     }
 });
+
+
+
+
+
