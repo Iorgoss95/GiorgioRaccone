e93277a924b418ef41492c6c09b8fe30fbb1b09f
giorgio <giorgio.raccone@finconsgroup.com>
2021-07-08 17:45:55 +0200

    fix media queries

--- js/script.js
+++ js/script.js
@@ -22,6 +22,10 @@ btn.addEventListener("click", function () {
         document.body.classList.remove("theme-" + (i))
         i = 2;
         document.body.classList.add("theme-" + (i - 1));
+        //get backgrund color and set to content property of meta tag
+    let color = window.getComputedStyle(document.querySelector(".top-container")).backgroundColor;
+    document.querySelector("meta[name='theme-color']").setAttribute("content", color);
+    
     }
     else {
         i++;
