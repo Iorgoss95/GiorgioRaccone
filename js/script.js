333b1f9c87a5be642994750c1e434dcb66d6b045
giorgio <giorgio.raccone@finconsgroup.com>
2021-07-12 12:24:48 +0200

    final version scrollbar & font-size refix

--- js/script.js
+++ js/script.js
@@ -35,11 +35,45 @@ btn.addEventListener("click", function () {
 
 
 //--DYNAMIC SCROLLBAR-----------------------------------
-const y = window.innerHeight*4;
-document.querySelector(".scrollbar").setAttribute("style", "height:"+ y +"px; transform: translateY("+ y +"vh)");
-window.addEventListener('scroll',(event) => {
-    document.querySelector(".scrollbar").setAttribute("style", "height:"+ y +"px; transform: translateY("+ -(window.innerHeight-pageYOffset) +"rem)");
-});
+var winheight, docheight, trackLength, throttlescroll;
+ 
+function getDocHeight() {
+    var D = document;
+    return Math.max(
+        D.body.scrollHeight, D.documentElement.scrollHeight,
+        D.body.offsetHeight, D.documentElement.offsetHeight,
+        D.body.clientHeight, D.documentElement.clientHeight
+    )
+}
+
+function getmeasurements(){
+    winheight= window.innerHeight || (document.documentElement || document.body).clientHeight
+    docheight = getDocHeight()
+    trackLength = docheight - winheight
+}
+ 
+function amountscrolled(){
+    var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
+    var pctScrolled = Math.floor(scrollTop/trackLength * 100) // gets percentage scrolled (ie: 80 or NaN if tracklength == 0)
+    console.log(pctScrolled + ' num % scrolled')
+    return pctScrolled;
+}
+ 
+getmeasurements()
+ 
+window.addEventListener("resize", function(){
+    getmeasurements()
+}, false)
+ 
+window.addEventListener("scroll", function(){
+    clearTimeout(throttlescroll)
+        throttlescroll = setTimeout(function(){ // throttle code inside scroll to once every 50 milliseconds
+        amountscrolled()
+    }, 50)
+
+    document.querySelector(".scrollbar").setAttribute("style", "transform: translateY("+( amountscrolled() -100)+"vh)");
+
+}, false)
 
     
 
