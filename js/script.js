fc02b4a2f37bf9077c07604396be5a2dd17f3c98
giorgio <giorgio.raccone@finconsgroup.com>
2021-07-12 17:36:34 +0200

    addEventListener for multi items

--- js/script.js
+++ js/script.js
@@ -6,31 +6,35 @@
 let color = window.getComputedStyle(document.querySelector(".top-container")).backgroundColor;
 document.querySelector("meta[name='theme-color']").setAttribute("content", color);
 
-const btn = document.querySelector(".btn-toggle");
+const btn = document.querySelectorAll(".btn-toggle");
 let i = 2;
-btn.addEventListener("click", function () {
 
+function clickButton() {
     document.body.classList.remove("theme-" + (i - 1))
     document.body.classList.add("theme-" + i);
-
+    
     //get backgrund color and set to content property of meta tag
     let color = window.getComputedStyle(document.querySelector(".top-container")).backgroundColor;
     document.querySelector("meta[name='theme-color']").setAttribute("content", color);
-
+    
     if (i > 5) {
         //i > of number oh themes in _colors.scss file
         document.body.classList.remove("theme-" + (i))
         i = 2;
         document.body.classList.add("theme-" + (i - 1));
         //get backgrund color and set to content property of meta tag
-    let color = window.getComputedStyle(document.querySelector(".top-container")).backgroundColor;
-    document.querySelector("meta[name='theme-color']").setAttribute("content", color);
-    
+        let color = window.getComputedStyle(document.querySelector(".top-container")).backgroundColor;
+        document.querySelector("meta[name='theme-color']").setAttribute("content", color);
+        
     }
     else {
         i++;
     }
-});
+}
+
+btn.forEach(item => {
+    item.addEventListener("click", clickButton)
+})
 
 
 
