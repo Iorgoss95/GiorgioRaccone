98856d5d8534639b61938309875f86888ae1900a
giorgio <giorgio.raccone@finconsgroup.com>
2021-07-13 16:20:21 +0200

    fisrt grid implementation

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ function clickButton() {
     let color = window.getComputedStyle(document.querySelector(".top-container")).backgroundColor;
     document.querySelector("meta[name='theme-color']").setAttribute("content", color);
     
-    if (i > 5) {
+    if (i > 6) {
         //i > of number oh themes in _colors.scss file
         document.body.classList.remove("theme-" + (i))
         i = 2;
